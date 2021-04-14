ba3d5f80094409ac0a95bd2d4b99c2d7edc23bda
srishti2007 <64143148+srishti2007@users.noreply.github.com>
2021-04-14 15:57:56 +0530

    Add files via upload

---
+++ screens/HomeScreen.js
@@ -0,0 +1,106 @@
+import React, { Component } from "react";
+import {
+  View,
+  StyleSheet,
+  Text,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import { ListItem } from "react-native-elements";
+import db from "../config";
+
+export default class HomeScreen extends Component {
+  constructor() {
+    super();
+    this.state = {
+      allRequests: [],
+    };
+    this.requestRef = null;
+  }
+
+  getAllRequests = () => {
+    this.requestRef = db
+      .collection("exchange_requests")
+      .onSnapshot((snapshot) => {
+        var allRequests = [];
+        snapshot.forEach((doc) => {
+          allRequests.push(doc.data());
+        });
+        this.setState({
+          allRequests: allRequests,
+        });
+      });
+  };
+
+  
+
+  keyExtractor = (item, index) => index.toString();
+
+  renderItem = ({ item, i }) => {
+    console.log(item.item_name)
+    return (
+      <ListItem
+        key={i}
+        title={item.item_name}
+        subtitle={item.item_description}
+        titleStyle={{ color: "black", fontWeight: "bold" }}
+        rightElement={
+          <TouchableOpacity style={styles.button}>
+            <Text style={{ color: "#ffff" }}>View</Text>
+          </TouchableOpacity>
+        }
+        bottomDivider
+      />
+    )
+  }
+  componentDidMount() {
+    this.getAllRequests();
+  }
+
+  componentWillUnmount() {
+    this.requestRef();
+  }
+
+
+  render() {
+    return (
+      <View style={{ flex: 1, backgroundColor: "rgb(236, 236, 236)" }}>
+        <View style={{ flex: 1 }}>
+          {this.state.allRequests.length === 0 ? (
+            <View style={styles.subContainer}>
+              <Text style={{ fontSize: 20 }}>List Of All Barter</Text>
+            </View>
+          ) : (
+            <FlatList
+              keyExtractor={this.keyExtractor}
+              data={this.state.allRequests}
+              renderItem={this.renderItem}
+            />
+          )}
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  subContainer: {
+    flex: 1,
+    fontSize: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    fontColor: "black",
+  },
+  button: {
+    width: 100,
+    height: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#ff5722",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 8,
+    },
+  },
+});
